950813b15d3b757f6444044473b812523011db6a
Gustavo Jesus <126497803+gusz11@users.noreply.github.com>
2023-09-14 10:31:09 -0300

    Update

--- projetos/pum.c
+++ projetos/pum.c
@@ -2,13 +2,15 @@
 
 int main(void)
 {
-    int valor;
+    int valor, inicio = 1;
 
     printf("Insira um valor aqui:\n");
     scanf("%d", &valor);
 
-    for (int i = 1; i <= valor; i++)
+    for (int i = 0; i < valor; i++)
     {
-        printf("%d %d %d PUM\n", i, i, i);
+        printf("%d %d %d PUM\n", inicio, inicio + 1, inicio + 2);
+        inicio = inicio + 4;
     }
+    return 0;
 }
